c2a3fa96d306ca0fb1772d0540ed721e9e49c558
Ioana Cretiu <icretiu@pitechnologies.ro>
2015-05-07 14:07:53 +0300

    Modules, routes and factories 48:29 wip.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 var myApp = angular.module('myApp',[]);
 
+myApp.config(function($routeProvider) {
+  $routeProvider
+    .when('/',
+      {
+        controller: 'SimpleController',
+        templateUrl: 'partials/View1.html'
+      })
+    .when ('/partial2',
+      {
+        controller: 'SimpleController',
+        templateUrl: 'partials/View2.html'
+      })
+    .otherwise({ redirectTo: '/'});
+});
+
 myApp.controller('SimpleController', ['$scope', function($scope) {
   $scope.customers = [
     {name:'Joe', city:'Bo'},
